5cbef0a88967fc1c8581feb31362cf1bb936cf7d
pedro fernandes <thephfernandes@gmail.com>
2020-12-18 05:12:33 +0100

    adds subheaders, equation (1), text emphasis, file formatting

--- pages/en/Linalg/basis.vue
+++ pages/en/Linalg/basis.vue
@@ -17,20 +17,26 @@
         <div class="inline" ref="MD_invertibleMatrix">
           The columns of an invertible $\mathbb{n} \times \mathbb{n}$ form a
           basis for all of $\mathbb{R}^\mathbb{n}$ because they are linearly
-          independent and span $\mathbb{R}^\mathbb{n}$, by the 
-          <nuxt-link :to="{ path: 'matrix_invers' }">Invertible Matrix Theorem.</nuxt-link>
-          One such matrix is the $\mathbb{n} \times \mathbb{n}$ identity matrix. Its columns
-          are denoted by $e_1$, ..., $e_n$:
+          independent and span $\mathbb{R}^\mathbb{n}$, by the
+          <nuxt-link :to="{ path: 'matrix_invers' }"
+            >Invertible Matrix Theorem.</nuxt-link
+          >
+          One such matrix is the $\mathbb{n} \times \mathbb{n}$ identity matrix.
+          Its columns are denoted by $e_1$, ..., $e_n$:
         </div>
         <p class="formel" ref="MD_identityVectors">
-            $e_1 = \begin{bmatrix} 1 \\ 0 \\ . \\. \\. \\ 0 \end{bmatrix}$,
-            $e_2 = \begin{bmatrix} 0 \\ 1 \\ . \\. \\. \\ 0 \end{bmatrix}$,
-            $e_n = \begin{bmatrix} 0 \\ 0 \\ . \\. \\. \\ n \end{bmatrix}$
+          $e_1 = \begin{bmatrix} 1 \\ 0 \\ . \\. \\. \\ 0 \end{bmatrix}$, $e_2 =
+          \begin{bmatrix} 0 \\ 1 \\ . \\. \\. \\ 0 \end{bmatrix}$, $e_n =
+          \begin{bmatrix} 0 \\ 0 \\ . \\. \\. \\ n \end{bmatrix}$
         </p>
         <div class="inline" ref="MD_standardBasis">
-          The set ${e_1, ..., e_n}$ is called the <b>standard basis</b> for $\mathbb{R}^\mathbb{n}$.
+          The set ${e_1, ..., e_n}$ is called the <b>standard basis</b> for
+          $\mathbb{R}^\mathbb{n}$.
         </div>
 
+        <div class="inline">
+          <h2>Finding the basis of a null space</h2>
+        </div>
         <p class="formel">
           Example: Find a basis for the null space of the matrix
         </p>
@@ -43,8 +49,8 @@
           First, write the solution of $Ax = 0$ in parametric vector form:
         </p>
         <p class="formel" ref="MD_3">
-          $\begin{bmatrix} A & 0 \end{bmatrix}$ ~ $\begin{bmatrix} 1 & -2 & 0 & -1
-          & 3 & 0\\ 0 & 0 & 1 & 2 & -2 & 0\\ 0 & 0 & 0 & 0 & 0 & 0
+          $\begin{bmatrix} A & 0 \end{bmatrix}$ ~ $\begin{bmatrix} 1 & -2 & 0 &
+          -1 & 3 & 0\\ 0 & 0 & 1 & 2 & -2 & 0\\ 0 & 0 & 0 & 0 & 0 & 0
           \end{bmatrix}, \begin{matrix} x_1 - 2x_2 - x_4 + 3x_5 = 0\\ x_3 + 2x_4
           - 2x_5 = 0\\ 0 = 0 \end{matrix}$
         </p>
@@ -53,11 +59,15 @@
           with $x_2, x_4,$ and $x_5$ free.
         </p>
         <p class="formel" ref="MD_exampleBasisSolution">
-          $\begin{bmatrix} x_1 \\ x_2 \\ x_3 \\ x_4 \\x_5\end{bmatrix} = 
-          \begin{bmatrix} 2x_2 + x_4 -3x_5 \\ x_2 \\-2x_4 + 2x_5 \\x_4\\x_5\end{bmatrix} =
-          x_2 \begin{bmatrix} 2 \\ 1 \\ 0 \\ 0 \\0 \end{bmatrix} +
-          x_4 \begin{bmatrix} 1 \\ 0 \\ -2 \\ 1 \\ 0 \end{bmatrix} +
-          x_5 \begin{bmatrix} -3 \\ 0 \\ 2 \\ 0 \\ 1 \end{bmatrix}$
+          $\begin{bmatrix} x_1 \\ x_2 \\ x_3 \\ x_4 \\x_5\end{bmatrix} =
+          \begin{bmatrix} 2x_2 + x_4 -3x_5 \\ x_2 \\-2x_4 + 2x_5
+          \\x_4\\x_5\end{bmatrix} = x_2 \begin{bmatrix} 2 \\ 1 \\ 0 \\ 0 \\0
+          \end{bmatrix} + x_4 \begin{bmatrix} 1 \\ 0 \\ -2 \\ 1 \\ 0
+          \end{bmatrix} + x_5 \begin{bmatrix} -3 \\ 0 \\ 2 \\ 0 \\ 1
+          \end{bmatrix}$
+        </p>
+        <p class="formel" ref="MD_equation1">
+          $= x_2 \textbf{u} + x_4 \textbf{v} + x_5 \textbf{w} (1)$
         </p>
         <p class="inline" ref="MD_5">
           Equation (1) shows that Nul $A$ coincides with the set of all linear
@@ -71,35 +81,41 @@
           for the null space. However, the method requires some explanation.
           Let’s begin with a simple case.
         </p>
-        <p class="inline" ref="MD_exampleColumnSpaceHeader">
+        <div class="inline">
+          <h2>Basis of a column space</h2>
+        </div>
+        <p class="formel" ref="MD_exampleColumnSpaceHeader">
           Find a basis for the column space of the matrix
         </p>
         <p class="formel" ref="MD_exampleColumnSpaceMatrix">
-          $B = \begin{bmatrix} 
-            1 & 0 & -3 & 5 & 0\\
-            0 & 1 & 2 & -1 & 0\\
-            0 & 0 & 0 & 0 & 1\\
-            0 & 0 & 0 & 0 & 0\\
-            \end{bmatrix}$
+          $B = \begin{bmatrix} 1 & 0 & -3 & 5 & 0\\ 0 & 1 & 2 & -1 & 0\\ 0 & 0 &
+          0 & 0 & 1\\ 0 & 0 & 0 & 0 & 0\\ \end{bmatrix}$
         </p>
         <p class="inline" ref="MD_6">
           We denote the columns of $\mathbb{B}$ by $b_1, ..., b_5$ and note that
-          $b_3 = -3b_1 + 2b_2$ and $b_4 = 5b_1 - b_2$. The fact that $b_3$ and $b_4$ are
-          combinations of the pivot columns mean that any combination of $b_1, ..., b_5$ is 
-          actually just a combination of $b_1, b_2, b_5$. Indeed, if $v$ is any vector in Col $B$, say;
+          $b_3 = -3b_1 + 2b_2$ and $b_4 = 5b_1 - b_2$. The fact that $b_3$ and
+          $b_4$ are combinations of the pivot columns mean that any combination
+          of $b_1, ..., b_5$ is actually just a combination of $b_1, b_2, b_5$.
+          Indeed, if $v$ is any vector in Col $B$, say;
         </p>
         <p class="formel" ref="MD_7">
-          $v = c_1 b_1 + c_2 b_2 + c_3 (-3b_1 + 2b_2) + c_4(5b_1 - b_2) + c_5 b_5$
+          $\textbf{v} = c_1 b_1 + c_2 b_2 + c_3 (-3b_1 + 2b_2) + c_4(5b_1 - b_2)
+          + c_5 b_5$
         </p>
         <p class="inline" ref="MD_8">
-          which is a linear combination of $b_1, b_2$, and $b_5$. So, ${b_1, b_2, b_5}$ spans Col $B$. Also,
-          $b_1$, $b_2$, and $b_5$ are linearly independent, because they are columns from an identity matrix.
-          So the pivot columns of $B$ form a basis for Col$B$. The matrix $B$ in the previous example is in
-          reduced echelon form. To handle a general matrix $A$, recall that linear dependence relations among the
-          columns of $A$ can be expressed in the form $Ax = 0$ for some $x$. (If some columns are not involved in a 
-          particular dependence relation, then the corresponding entries in $x$ are zero.) When $A$ is row reduced to 
-          echelon form $B$, the columns are drastically changed, but the equations $Ax = 0$ and $Bx = 0$ have the same set
-          of solutions. That is, the columns of $A$ have exactly the same linear dependence relationships as the columns of $B$!
+          which is a linear combination of $b_1, b_2$, and $b_5$. So, ${b_1,
+          b_2, b_5}$ spans Col $B$. Also, $b_1$, $b_2$, and $b_5$ are linearly
+          independent, because they are columns from an identity matrix. So the
+          pivot columns of $B$ form a basis for Col$B$. The matrix $B$ in the
+          previous example is in reduced echelon form. To handle a general
+          matrix $A$, recall that linear dependence relations among the columns
+          of $A$ can be expressed in the form $Ax = 0$ for some $x$. (If some
+          columns are not involved in a particular dependence relation, then the
+          corresponding entries in $x$ are zero.) When $A$ is row reduced to
+          echelon form $B$, the columns are drastically changed, but the
+          equations $Ax = 0$ and $Bx = 0$ have the same set of solutions. That
+          is, the columns of $A$ have $exactly\space the\space same\space linear\space dependence
+          relationships$ as the columns of $B$.
         </p>
       </template>
       <template v-slot:footer> </template>
